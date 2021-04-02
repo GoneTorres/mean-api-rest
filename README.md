f43aff0971f6885e9e5336d3351a262bd1ea0310
TORRES TONKOWICZ, GONZALO MARTIN <gonzalotorrestz@gmail.com>
2021-04-02 17:18:21 -0300

    Added CORS configuration

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # mean-api-rest
 Api REST created with MEAN stack (Following Victor Robles' Udemy course "Master en JavaScript: Aprender JS, jQuery, Angular, NodeJS")
+
+The final product is a Portfolio where you can upload your "projects".
+
+# NodeJs Backend Created
+- CRUD Operations for "Projects" created.
+- Projects can contain image files.
+- Added CORS configuration
--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // CORS
+app.use((request, response, next) => {
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    response.header('Access-Control-Allow-Origin', 'GET, POST, OPTIONS, PUT, DELETE');
+    response.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 // Routes
 app.use('/project', projectRoutes);
